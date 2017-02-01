246db6375e9e1fa01a1ced4215ec2be85b153343
VanHakobyan <van19962013@mail.ru>
2017-02-01 21:06:49 +0400

    add

--- Shape3D/Tessting/Program.cs
+++ Shape3D/Tessting/Program.cs
@@ -12,15 +12,15 @@ namespace Tessting
             Console.WriteLine(elips.areaGet());    // area ellipsoidal
             Console.WriteLine(elips.massGet());    // mass ellipsoidal
             Console.WriteLine(elips.volumeGet());  // volume ellipsoidal
-            elips.MoveBy(5f, 1.5f, 4.1f);
-            elips.MoveTo(6.7f, 4f, 3f);
+            elips.MoveBy(5f, 1.5f, 4.1f);            // moveing BY
+            elips.MoveTo(6.7f, 4f, 3f);              // moveing to
 
             parallelepiped paral = new parallelepiped(1.5f, 5, 15, 800); //parameters
             Console.WriteLine(paral.areaGet());     //area parallelogram
             Console.WriteLine(paral.massGet());     //mass parallelogram
             Console.WriteLine(paral.volumeGet());   //volume parallelogram
-            paral.MoveBy(4.1f, 4f, 2.9f);
-            paral.MoveTo(5.3f,1.1f,3.8f);
-        }
+            paral.MoveBy(4.1f, 4f, 2.9f);          // moveing BY
+            paral.MoveTo(5.3f,1.1f,3.8f);            // moveing to
+        } 
     }
 }
